cecf09ef822d149599ed170cd344c589dde73da6
wormholeracing <andyjm9@gmail.com>
2016-08-26 09:33:54 -0400

    Project setup

---
+++ src/main/java/com/ssa/ironyard/SudokuGame.java
@@ -0,0 +1,22 @@
+package com.ssa.ironyard;
+
+public class SudokuGame{
+    private final int id;
+    private final String difficulty;
+    private final int[] game = new int[81];
+    private int[] solution = new int[81];
+    
+    public SudokuGame(int id, String difficulty, String game) {
+	if(!(game.matches("\\d+") && game.length() == 81))
+		throw new IllegalArgumentException();
+	
+	this.id = id;
+	this.difficulty = difficulty;
+	for(int i = 0; i < game.length(); i++)
+	    this.game[i] = Integer.parseInt(game.substring(i, 1));
+	
+	this.solution = new SudokuSolverImpl(this).solve();
+    }
+    
+    
+}
---
+++ src/main/java/com/ssa/ironyard/SudokuSolverImpl.java
@@ -0,0 +1,16 @@
+package com.ssa.ironyard;
+
+public class SudokuSolverImpl implements SudokuSolver
+{
+    private final SudokuGame s;
+
+    public int[] solve() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    public SudokuSolverImpl(SudokuGame s) {
+	this.s = s;
+    }
+
+}
